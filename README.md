f86fb1a4a49580bcc19f699eaad98e9ebb24ef38
Aaron Weinberg <aaronw3373@gmail.com>
2016-06-29 16:52:12 -0400

    readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # scan_platform
 ros related code to control a platform for 3D scanning of objects
 
-# Run Test:
+ # Run Test:
  - roscore
  - sudo chmod a+rw /dev/ttyACM0 or USB0 whichever shows
- - rosrun rosserial_python serial_node.py /dev/ttyACM0 (or USB0)
+ - rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 (or USB0)
  - rostopic pub --once /scan_increment std_msgs/Float64 "data: 180.0"
 
 
@@ -12,4 +12,22 @@ ros related code to control a platform for 3D scanning of objects
  - roscore
  - sudo chmod a+rw /dev/ttyACM0 or USB0 whichever shows
  - rosrun rosserial_python serial_node.py /dev/ttyACM0 (or USB0)
+ - roslaunch src/scan_platform/ros_stepper_scan/stepper_scan.launch 
+ - roslaunch kinect2_bridge kinect2_bridge.launch (may have to run off luc's see notes)
  - rosservice call /start_stepper_scan "model_name: 'put_name_of_object_here'"
+
+
+
+Notes:
+1. if error connecting to arduino when:
+rosrun rosserial_python serial_node.py /dev/ttyACM0
+resart computer
+
+2. if rostopic list doesn't show all the kinect topics then
+su luc
+password: JinxAbbyMerry
+roslaunch kinect2_bridge kinect2_bridge.launch
+
+3. to view point cloud file 
+pcl_viewer catkin_ws/devel/lib/ros_stepper_scan/<file-name>
+
---
+++ ros_stepper_scan/src/single_scan.cpp
@@ -0,0 +1,57 @@
+// make topic for taking a single scan
+
+// /*
+//  *  stepper_scan.cpp
+//  *
+//  *  Copyright (c) 2016, Luc Bettaieb
+//  *  BSD Licensed
+//  */
+
+// #include <ros/ros.h>
+// #include <ros_stepper_scan/kinect2_interface.h>
+// #include <angles/angles.h>
+// #include <boost/shared_ptr.hpp>
+// #include <std_msgs/Float64.h>
+// #include "ros_stepper_scan/stepper_scan.h"
+
+// ros::Publisher scan_increment_pub;
+// boost::shared_ptr<Kinect2Interface> kinect;
+
+// double g_increment_degrees, g_increment_radians;
+// int g_n_snaps;
+
+// bool scan(ros_stepper_scan::stepper_scan::Request &request,
+//           ros_stepper_scan::stepper_scan::Response &response)
+// {
+//   std_msgs::Float64 increment;
+//   increment.data = g_increment_degrees;
+//   for (double ang = 0; ang < 360; ang += g_increment_degrees)
+//   {
+//     scan_increment_pub.publish(increment);
+//     for (size_t i = 0; i < g_n_snaps; i++)
+//       kinect->snapshot(request.model_name, i, ang);
+//   }
+// }
+
+// int main(int argc, char** argv)
+// {
+//   ros::init(argc, argv, "stepper_scan");
+//   ros::NodeHandle nh;
+
+//   kinect.reset(new Kinect2Interface(nh));
+
+//   if (!nh.getParam("ros_stepper_scan/increment_degrees", g_increment_degrees))
+//     g_increment_degrees = 10.0;  // Default value
+//   if (!nh.getParam("ros_stepper_scan/snaps_per_increment", g_n_snaps))
+//     g_n_snaps = 1;  // Default value
+
+
+//   g_increment_radians = angles::from_degrees(g_increment_radians);
+
+
+//   ros::ServiceServer stepper_scan = nh.advertiseService("start_stepper_scan", scan);
+//   scan_increment_pub = nh.advertise<std_msgs::Float64>("scan_increment", 10);
+
+
+//   ros::spin();
+// }
